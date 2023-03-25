706ea46e85597c742597648b760e48bb7795e992
xauravww <sauravmaheshwari8@gmail.com>
2023-03-26 01:41:12 +0530

    added spinner , added fetch queries for Feed

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
+import { client } from "../client"
+import { feedQuery, searchQuery } from "../utils/data"
+import MasonryLayout from "./MasonryLayout.jsx"
+import Spinner from "./Spinner.jsx"
 const Feed = () => {
-  return <div>Feed</div>
+  const [loading, setLoading] = useState(false)
+  const [pins, setPins] = useState(null)
+  const { categoryId } = useParams()
+
+  useEffect(() => {
+    setLoading(true)
+
+    if (categoryId) {
+      const query = searchQuery(categoryId)
+      client.fetch(query).then((data) => {
+        setPins(data) // another state
+        setLoading(false)
+      })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data)
+        setLoading(false)
+      })
+    }
+  }, [categoryId])
+
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed!" />
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>
 }
 
 export default Feed
