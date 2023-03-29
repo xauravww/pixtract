38012d4a2e9c8ffbce8448a95ad6cc9079769bd0
xauravww <sauravmaheshwari8@gmail.com>
2023-03-29 22:45:17 +0530

    utility functions updated

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -66,28 +66,83 @@ export const categories = [
   }
 ]
 
-export const userQuery = (sub) => {
-  const query = `*[_type == "user" && _id == '${sub}']`
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+  _id,
+  destination,
+  postedBy->{
+    _id,
+    userName,
+    image
+  },
+  save[]{
+    _key,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+  },
+} `
+
+export const pinDetailQuery = (pinId) => {
+  const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`
   return query
 }
 
-export const searchQuery = (searchTerm) => {
-  const query = `*[_type == "pin && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*'  ]{
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
     image{
-      asset ->{
+      asset->{
         url
       }
     },
     _id,
     destination,
-    postedBy ->{
+    postedBy->{
       _id,
       userName,
       image
     },
     save[]{
       _key,
-      postedBy ->{
+      postedBy->{
         _id,
         userName,
         image
@@ -96,27 +151,85 @@ export const searchQuery = (searchTerm) => {
   }`
   return query
 }
-// asterisk added '${searchTerm}*' searches even before searching whole term
 
-export const feedQuery = `*[_type == "pin" ] | order(_createdAt desc){
-  image{
-    asset ->{
-      url
-    }
-  },
-  _id,
-  destination,
-  postedBy ->{
+// asterisk added '${searchTerm}*' searches even before searching whole term
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+    image{
+      asset->{
+        url
+      }
+    },
     _id,
-    userName,
-    image
-  },
-  save[]{
-    _key,
-    postedBy ->{
+    destination,
+    postedBy->{
+      _id,
+      userName,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+            },
+          }`
+  return query
+}
+
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`
+  return query
+}
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
       _id,
       userName,
       image
     },
-  },
-}`
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`
+  return query
+}
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`
+  return query
+}
