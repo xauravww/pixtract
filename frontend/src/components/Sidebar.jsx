e66fedcffb5cf749c493c92e5b1d88bb5ed4530d
xauravww <sauravmaheshwari8@gmail.com>
2023-03-31 17:37:04 +0530

    categories section updated with img

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,30 +3,12 @@ import { NavLink, Link } from "react-router-dom"
 import { RiHomeFill } from "react-icons/ri"
 import { IoIosArrowForward } from "react-icons/io"
 import logo from "../assets/logopix.png"
-
+import { categories } from "../utils/data"
 const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize"
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize"
 
-const categories = [
-  {
-    name: "Animals"
-  },
-  { name: "Wallpapers" },
-  {
-    name: "Photography"
-  },
-  {
-    name: "Gaming"
-  },
-  {
-    name: "Coding"
-  },
-  {
-    name: "Other"
-  }
-]
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false)
@@ -65,6 +47,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              />
               {category.name}
             </NavLink>
           ))}
