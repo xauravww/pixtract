a1e18046465a652b52b1bde83f50c24dc09aeb54
xauravww <sauravmaheshwari8@gmail.com>
2023-04-29 14:15:46 +0530

    Improved UI and delete comments functionality added

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -15,6 +15,20 @@ const PinDetail = ({ user }) => {
   const [AddingComment, setAddingComment] = useState(false)
 
   const { pinId } = useParams()
+
+  // Define the function to delete a comment from the current pin
+  const deleteComment = (commentId) => {
+    // Use the Sanity client to patch the current pin, removing the specified comment
+    client
+      .patch(pinId)
+      .unset([`comments[${commentId}]`])
+      .commit()
+      .then(() => {
+        console.log("ho gyi delete")
+        fetchPinDetails()
+      })
+  }
+
   const addComment = () => {
     if (comment) {
       setAddingComment(true)
@@ -28,7 +42,7 @@ const PinDetail = ({ user }) => {
             _key: uuidv4(),
             postedBy: {
               _type: "postedBy",
-              _ref: user._id
+              _ref: user?._id
             }
           }
         ])
@@ -55,6 +69,7 @@ const PinDetail = ({ user }) => {
     }
   }
   useEffect(() => {
+    console.log(user)
     fetchPinDetails()
   }, [pinId])
 
@@ -62,94 +77,128 @@ const PinDetail = ({ user }) => {
   return (
     <>
       <div
-        className="flex xl:flex-row flex-col m-auto bg-white "
-        style={{ maxWidth: "1500px", borderRadius: "32px" }}
+        className=""
+        // style={{ overflowY: "hidden" }}
       >
-        <div className="flex justify-center items-center md:items-start flex-initial">
-          <img
-            src={pinDetail?.image && urlFor(pinDetail.image).url()}
-            className="rounded-t-3xl rounded-b-lg"
-            alt="user-post"
-          />
-        </div>
-        <div className="w-full p-5 flex-1 xl:min-w-630">
-          <div className="flex items-center justify-between">
-            <div className="flex gap-2 items-center">
+        <div
+          className="flex xl:flex-row flex-col m-auto bg-white py-3 px-3 shadow-lg "
+          style={
+            {
+              // maxWidth: "1500px",
+              // borderRadius: "32px",
+              // minHeight: "calc(100vh - 4rem)"
+            }
+          }
+        >
+          <div className="flex justify-center items-center md:items-start flex-initial">
+            <img
+              src={pinDetail?.image && urlFor(pinDetail.image).url()}
+              className="h-full rounded-t-3xl rounded-b-lg"
+              alt="user-post"
+            />
+          </div>
+          <div className="w-full p-5 flex-1 xl:min-w-630">
+            <div className="flex items-center justify-between bg-gray-100 rounded-md p-3">
+              <div className="flex gap-2 items-center ">
+                <a
+                  href={`${pinDetail.image?.asset?.url}?dl=`}
+                  download
+                  className="bg-white w-10 h-10 rounded-full flex items-center justify-center text-dark text-2xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
               <a
-                href={`${pinDetail.image?.asset?.url}?dl=`}
-                download
-                className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                href={pinDetail.destination}
+                target="_blank"
+                rel="noreferrer"
+                className="text-sm font-medium text-gray-600 hover:text-gray-900"
               >
-                <MdDownloadForOffline />
+                {pinDetail.destination}
               </a>
             </div>
-            <a href={pinDetail.destination} target="_blank" rel="noreferrer">
-              {pinDetail.destination}
-            </a>
-          </div>
-          <div>
-            <h1 className="text-4xl font-bold break-words mt-3">
-              {pinDetail.title}
-            </h1>
-            <p className="mt-3">{pinDetail.about} </p>
-          </div>
-          <Link
-            to={`user-profile/${pinDetail.postedBy?._id}`}
-            className="flex gap-2 mt-5 items-center bg-white rounded-lg"
-          >
-            <img
-              className="w-8 h-8 rounded-full object-cover "
-              src={pinDetail?.postedBy?.image}
-              alt="user-profile"
-            />
-            <p className="font-semibold capitalize">
-              {pinDetail.postedBy?.userName}
-            </p>
-          </Link>
-          <h2 className="mt-5 text-2xl">Comments</h2>
-          <div className="max-h-370 overflow-y-auto">
-            {pinDetail?.comments?.map((comment, i) => (
-              <div
-                className="flex gap-2 mt-5 items-center bg-white rounded-lg"
-                key={i}
-              >
-                <img
-                  src={comment?.postedBy?.image}
-                  alt="user-profile"
-                  className="w-10 h-10 rounded-full cursor-pointer"
-                />
-                <div className="flex flex-col">
-                  <p className="font-bold">{comment.postedBy?.userName}</p>
-                  <p>{comment.comment}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-wrap mt-6 gap-3">
-            <Link to={`user-profile/${user._id}`}>
+
+            <div>
+              <h1 className="text-4xl font-bold break-words mt-3">
+                {pinDetail.title}
+              </h1>
+              <p className="mt-3">{pinDetail.about} </p>
+            </div>
+            <Link
+              to={`user-profile/${pinDetail.postedBy?._id}`}
+              className="flex gap-2 mt-5 items-center bg-white rounded-lg p-3 shadow-md"
+            >
               <img
-                className="w-10 h-10 rounded-full cursor-pointer "
-                src={user.image}
+                className="w-8 h-8 rounded-full object-cover "
+                src={pinDetail?.postedBy?.image}
                 alt="user-profile"
               />
+              <p className="font-semibold capitalize">
+                {pinDetail.postedBy?.userName}
+              </p>
             </Link>
-            <input
-              className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
-              type="text"
-              placeholder="Add a comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            />
-            <button
-              type="button"
-              className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
-              onClick={addComment}
-            >
-              {AddingComment ? "Posting..." : "Post"}
-            </button>
+            <h2 className="mt-5 text-2xl">Comments</h2>
+            <div className="max-h-370 overflow-y-auto">
+              {/* // Render the comments section, including a delete button for each
+            comment if the comment was posted by the current user */}
+              {pinDetail?.comments?.map((comment, i) => (
+                <div
+                  className="flex gap-2 mt-5 items-center bg-white rounded-lg p-3 shadow-md"
+                  key={i}
+                >
+                  <img
+                    src={comment?.postedBy?.image}
+                    alt="user-profile"
+                    className="w-10 h-10 rounded-full cursor-pointer"
+                  />
+                  <div className="flex justify-between items-center bg-gray-100 p-4 rounded-lg mb-4 w-full">
+                    <p className="text-gray-800 font-semibold">
+                      {comment.postedBy?.userName}
+                    </p>
+                    <div className="flex-1 ml-4">
+                      <p className="text-gray-700">{comment.comment}</p>
+                    </div>
+                    {comment.postedBy?._id === user?._id && (
+                      <button
+                        onClick={() => {
+                          deleteComment(i)
+                        }}
+                        className="bg-red-500 hover:bg-red-600 text-white rounded-full px-2 py-1 font-semibold text-sm outline-none flex-shrink-0"
+                      >
+                        Delete
+                      </button>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-wrap mt-6 gap-3">
+              <Link to={`user-profile/${user?._id}`}>
+                <img
+                  className="w-10 h-10 rounded-full cursor-pointer shadow-md"
+                  src={user?.image}
+                  alt="user-profile"
+                />
+              </Link>
+              <input
+                className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300 shadow-md"
+                type="text"
+                placeholder="Add a comment"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <button
+                type="button"
+                className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none shadow-md"
+                onClick={addComment}
+              >
+                {AddingComment ? "Posting..." : "Post"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
       {pins?.length > 0 && (
         <h2 className="text-center font-bold text-2xl mt-8 mb-4">
           More like this
--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Routes, Route } from "react-router-dom"
 
 import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components"
 const Pins = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState("") // we wil share it with other one componnets , so we use it here
+  const [searchHistory, setsearchHistory] = useState(false)
+  const inputRef = useRef(null)
+
+  const [inputValue, setInputValue] = useState("")
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
@@ -11,6 +15,9 @@ const Pins = ({ user }) => {
           searchTerm={searchTerm}
           setSearchTerm={setSearchTerm}
           user={user}
+          searchHistory={searchHistory}
+          setsearchHistory={setsearchHistory}
+          setInputValue={setInputValue}
         />
       </div>
       <div className="h-full">
@@ -25,7 +32,13 @@ const Pins = ({ user }) => {
           <Route
             path="/search"
             element={
-              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              <Search
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                searchHistory={searchHistory}
+                setsearchHistory={setsearchHistory}
+                inputValue={inputValue}
+              />
             }
           />
         </Routes>
